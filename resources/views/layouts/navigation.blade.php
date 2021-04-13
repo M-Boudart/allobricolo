e63c7942b582ed0dfc340904696632a5282207c1
Maxime Boudart <mboudartpro@gmail.com>
2021-04-13 15:23:12 +0200

    Ajout d'un lien dashboard dans le backend

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Site principale') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        Dashboard
+                    </x-nav-link>
+                </div>
                 @if (Auth::user()->status->status == 'Admin')
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('backend.user.index')" :active="request()->routeIs('backend.user.index')">
