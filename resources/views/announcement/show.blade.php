39289b117a41f31334fe471abebd0308542313a3
Maxime Boudart <mboudartpro@gmail.com>
2021-05-07 12:09:09 +0200

    Le modal report est mis dans une vue partielle

--- resources/views/announcement/show.blade.php
+++ resources/views/announcement/show.blade.php
@@ -9,11 +9,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Allo Bricolo</title>
 
-    <!-- Report Modal -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
-
-
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
 
@@ -165,31 +160,13 @@
             </div>
     </div>
 
-<!-- Report Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Veuillez expliquer la raison du signalement</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form action="{{ route('report.report', ['type' => 'announcements', 'id' => $announcement->id]) }}" method=post>
-        @csrf
-        <div class="mb-3">
-            <label for="reason" class="col-form-label">Raison:</label>
-            <textarea class="form-control" id="reason" name="reason"></textarea>
-        </div>
-        <button class="btn btn-danger">Signaler</button>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-      </div>
-    </div>
-  </div>
-</div>
-
+    @if (Auth::id() != $announcement->applicant_user_id)
+        @include('partials.reportModal', [
+            'type' => "announcements",
+            'ressourceId' => $announcement->id,
+        ])
+    @endif
+    
     @include('partials.footer')
     <!-- Js Plugins Template de base -->
     <script src="{{ asset('template/js/jquery-3.3.1.min.js') }}"></script>
@@ -202,9 +179,5 @@
     <script src="{{ asset('template/js/jquery.slicknav.js') }}"></script>
     <script src="{{ asset('template/js/owl.carousel.min.js') }}"></script>
     <script src="{{ asset('template/js/main.js') }}"></script>
-
-    <!-- Report Modal -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ resources/views/partials/reportModal.blade.php
@@ -0,0 +1,29 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Veuillez expliquer la raison du signalement</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form action="{{ route('report.report', ['type' => $type, 'id' => $ressourceId]) }}" method=post>
+        @csrf
+        <div class="mb-3">
+            <label for="reason" class="col-form-label">Raison:</label>
+            <textarea class="form-control" id="reason" name="reason"></textarea>
+        </div>
+        <button class="btn btn-danger">Signaler</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
