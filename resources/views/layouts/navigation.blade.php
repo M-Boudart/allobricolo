030b89ce3f3be05daba2ef7772992a4a7038fb46
M-Boudart <62714152+M-Boudart@users.noreply.github.com>
2021-10-05 21:31:51 +0200

    Update menu dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                    <a href="{{ route('welcome') }}">
+                        <img src="{{ asset('img/logo-black.png') }}" alt="Logo">
                     </a>
                 </div>
 
